88f7e942f1a2af72ebc2b2703a3cb35ed2f1fa95
Rodolfo Dev <99969980+rodolfocoprodutor@users.noreply.github.com>
2022-09-30 21:12:07 -0300

    Alteração de php mailer

--- envio.php
+++ envio.php
@@ -1,62 +1,58 @@
 <?php
+
+error_reporting(0);
+
+$nome = utf8_encode($_POST['nome']);
+$email = utf8_encode($_POST['email']);
+$whatsapp = utf8_encode($_POST['whatsapp']);
+$cep = utf8_encode($_POST['cep']);
+$rua = utf8_encode($_POST['rua']);
+$numero = utf8_encode($_POST['numero']);
+$cidade = utf8_encode($_POST['cidade']);
+$estado = utf8_encode($_POST['estado']);
+
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+
+$mail->Host       = 'mail.lesmains.com.br';
+$mail->SMTPSecure = "tls";
+$mail->Port       = 465;
+$mail->SMTPAuth   = true;
+$mail->Username   = 'revendedoras@lesmains.com.br';
+$mail->Password   = 'Za0ftaqr5qmT';
+
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+$mail->setFrom($mail->Username,"Lesmains");
+$mail->addAddress('revendedoras@lesmains.com.br');
+$mail->addReplyTo('revendedoras@lesmains.com.br');
+$mail->Subject = "Novo cadastro em revendedora Lesmains";
 
-if (isset($_POST['enviar'])) {
+$mail->isHTML(true);                                  //Set email format to HTML
 
-    //Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+$msg = "
+        Nome: $nome
+        E-mail: $email
+        Whatsapp: $whatsapp
+        CEP: $cep
+        Rua: $rua
+        Número: $numero
+        Cidade: $cidade
+        Estado: $estado
+        
+        ";
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'mail.lesmains.com.br';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'revendedoras@lesmains.com.br';                     //SMTP username
-        $mail->Password   = 'Za0ftaqr5qmT';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+$mail->Body = $msg;
 
-        //Recipients
-        $mail->setFrom('revendedoras@lesmains.com.br', 'Mailer');
-        $mail->addAddress('revendedoras@lesmains.com.br', 'Lesmains');     //Add a recipient
-        //$mail->addAddress('ellen@example.com');               //Name is optional
-        $mail->addReplyTo('revendedoras@lesmains.com.br', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-
-        //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-
-        $body = "Nome: ". $_POST['nome']."<br>";
-                "E-mail: ". $_POST['email']."<br>";
-                "Whatsapp: ". $_POST['whatsapp']."<br>";
-                "CEP: ". $_POST['cep']."<br>";
-                "Rua: ". $_POST['rua']."<br>";
-                "Número: ". $_POST['numero']."<br>";
-                "Cidade: ". $_POST['cidade']."<br>";
-                "Estado: ". $_POST['estado'];        
-
-        $mail->Body    = $body;
-        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-        echo 'Dados de cadastro enviado com sucesso';
-    } catch (Exception $e) {
-        echo "Erro ao enviar dados de cadastro: {$mail->ErrorInfo}";
-    }
-}else {
-    echo "Erro ao enviar email pelo link use a página do site"
+if ( $mail->send()){
+    echo "email enviado com sucesso!";
+} else {
+    echo "erro ao enviar o email" . $mail->ErrorInfo;
 }
