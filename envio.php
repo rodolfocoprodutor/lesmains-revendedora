55e91ddf267f939345f00aa93b04f80af21bc277
Rodolfocoprodutor <rodolfocoprodutor@gmail.com>
2022-10-01 23:45:37 -0300

    segundo teste do phpmailer

--- envio.php
+++ envio.php
@@ -1,37 +1,46 @@
 <?php
-error_reporting(0);
-
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-$nome = utf8_encode($_POST['nome']);
-
+//Load Composer's autoloader
 require 'vendor/autoload.php';
 
-$phpmailer = new PHPMailer();
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $phpmailer = new PHPMailer();
 $phpmailer->isSMTP();
 $phpmailer->Host = 'smtp.mailtrap.io';
 $phpmailer->SMTPAuth = true;
 $phpmailer->Port = 2525;
 $phpmailer->Username = '127efe3c64258b';
-$phpmailer->Password = '3010d093d9e6c5';
+$phpmailer->Password = '3010d093d9e6c5';                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-$mail->setFrom($mail->Username,"Lesmains");
-$mail->addAddress('revendedoras@lesmains.com.br');
-$mail->addReplyTo('revendedoras@lesmains.com.br');
-$mail->Subject = "Novo cadastro em revendedora Lesmains";
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
 
-$mail->isHTML(true);                                  //Set email format to HTML
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-$msg = "Nome:" . $nome;
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-$mail->Body = $msg;
-
-if ( $mail->send()){
-    echo "email enviado com sucesso!";
-} else {
-    echo "erro ao enviar o email" . $mail->ErrorInfo;
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
